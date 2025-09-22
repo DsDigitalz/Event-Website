ed24cef39b3a36660c8f2c7e2aa18c402d651235
Daniel Sanu <sanudaniel46@gmail.com>
2025-09-21 21:05:17 -1000

    modified component EventCards.jsx

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function AboutSection() {
   return (
     <section className="relative bg-white px-4 py-20">
-      <img src="aboutimg2.png" alt="" className="absolute left-0 bottom-0" />
+      <img src="aboutimg2.png" alt="" className="absolute left-0 bottom-5 w-20" />
       <img src="aboutimg.png" alt="" className="absolute right-0 top-0" />
       <div className="px-4 lg:px-10 max-w-[1440px] mx-auto flex flex-col items-center justify-between space-y-4 md:space-y-4 lg:flex-row ">
         <h2 className="text-center text-3xl md:text-4xl font-extrabold text-[#111111]">
--- src/Components/EventCards.jsx
+++ src/Components/EventCards.jsx
@@ -3,28 +3,25 @@ import React from "react";
 function EventCards() {
   const events = [
     {
-      image:
-        "https://images.unsplash.com/photo-1538332576161-b76543b591b6?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: "eventcard1.png",
       category: "MEMORABLE EXPERIENCE",
       title: "Unforgettable Moments at Eventive 2025",
     },
     {
-      image:
-        "https://images.unsplash.com/photo-1596701802931-e44b82650022?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: "eventcard2.png",
       category: "SKILLED SPEAKERS",
       title: "Storytelling Festival",
     },
     {
-      image:
-        "https://images.unsplash.com/photo-1531051759657-6b455b55239e?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: "eventcard3.png",
       category: "COMMUNITY BUILD",
       title: "Build Networking",
     },
   ];
 
   return (
-    <div className="bg-white py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <div className=" bg-white py-3 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-[1440px] lg:px-10 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {events.map((event, index) => (
           <div
             key={index}
